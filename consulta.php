9125786fbfb91ae154abdd5610a2ae7ca037e62f
sebastianpolito13 <104749693+sebastianpolito13@users.noreply.github.com>
2022-05-08 17:09:25 -0300

    Add files via upload

--- consulta.php
+++ consulta.php
@@ -1,14 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
 
-<?php
+	<?php
 
-	$Nombre = $_POST ['Nombre'];
-	$email = $_POST ['email'];
-	$password = $_POST ['password'];
-	$Ciudad = $_POST ['Ciudad'];
-	$Pais = $_POST ['pais'];
-	mail ($Nombre, $email, $password, $Ciudad, $Pais);
+		$Nombre = $_POST ['Nombre'];
+		$email = $_POST ['email'];
+		$password = $_POST ['password'];
+		$Ciudad = $_POST ['Ciudad'];
+		$Pais = $_POST ['pais'];
+		mail ($Nombre, $email, $password, $Ciudad, $Pais);
 
-	print ("<h3> Muchas gracias por su mensaje uwu</h3>");
+		print ("<h3> Muchas gracias por su mensaje</h3>");
 	
-?>
+	?>
+
+</body>
+</html>
 
